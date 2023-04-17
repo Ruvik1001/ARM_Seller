44407c3862fdbd88b75d9310d7d4384fb5a7c78c
Rudnev Viktor <rudnevvictor2003@mail.ru>
2023-04-17 16:50:03 +0300

    end2

--- app/src/main/java/com/ruvik1001/arm_seller/ReportAboutDefectActivity.java
+++ app/src/main/java/com/ruvik1001/arm_seller/ReportAboutDefectActivity.java
@@ -37,11 +37,11 @@ public class ReportAboutDefectActivity extends AppCompatActivity {
         });
     }
 
-
+    /*
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
             Bitmap photo = (Bitmap) data.getExtras().get("data");
             //now we can put our photo anyway
         }
-    }
+    }*/
 }
